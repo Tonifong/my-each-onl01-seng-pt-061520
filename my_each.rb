4e00de6f7c6aa271924114ac627393e131c71b27
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 21:45:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,31 +1,3 @@
-def my_each(array) # put argument(s) here
-i = 0
-while i < array.length
-  yield(array[i])
-  i = i + 1
+def my_each # put argument(s) here
+  # code here
 end
-array
-end (edited) 
-2:38
-yield and return lab: require 'pry'
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" } (edited) 
-2:39
-my collect lab:
-def my_collect(array)
-    i = 0
-    name_collection = []
-    while i < array.length
-      name_collection.push yield(array[i])
-      i += 1
-    end
-    name_collection
